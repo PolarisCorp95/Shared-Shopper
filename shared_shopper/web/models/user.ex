9698da371553864cc931a939a759379d564d6df6
padukkas <sathira.padukka@gmail.com>
2017-05-04 20:03:36 -0400

    User accounts

--- shared_shopper/web/models/user.ex
+++ shared_shopper/web/models/user.ex
@@ -1,21 +1,22 @@
 defmodule SharedShopper.User do
-  use SharedShopper.Web, :model
+ use SharedShopper.Web, :model
 
-  schema "users" do
-    field :email, :string
-    field :password_hash, :string
-    field :name, :string
-    field :username, :string
+ schema "users" do
+   field :email, :string
+   field :password_hash, :string
+   field :name, :string
+   field :username, :string
 
-    timestamps()
-  end
+   timestamps()
+ end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
-  def changeset(struct, params \\ %{}) do
+ @doc """
+ Builds a changeset based on the `struct` and `params`.
+ """
+   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:email, :password_hash, :name, :username])
     |> validate_required([:email, :password_hash, :name, :username])
+    |> validate_format(:email, ~r/@/)
   end
 end
