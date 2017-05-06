68f381b1d67c1fade6b8017133b3e03019ad9b1e
padukkas <sathira.padukka@gmail.com>
2017-05-06 16:35:49 -0400

    Comeonin Installation

--- shared_shopper/web/controllers/user_controller.ex
+++ shared_shopper/web/controllers/user_controller.ex
@@ -1,24 +1,48 @@
 defmodule SharedShopper.UserController do
-  use SharedShopper.Web, :controller
-  alias SharedShopper.User
-  def index(conn, _params) do
-    users = Repo.all(User)
-    render(conn, "index.html", users: users)
-  end
-  def new(conn, _params) do
-  changeset = User.changeset(%User{})
-  render(conn, "new.html", changeset: changeset)
-  end
-  def create(conn, %{"user" => user_params}) do
-  changeset = User.changeset(%User{}, user_params)
-  case Repo.insert(changeset) do
-    {:ok, user} ->
-      conn
-      |> put_flash(:info, "User created!")
-      |> redirect(to: user_path(conn, :index))
-    {:error, changeset} ->
-      conn
-      |> render("new.html", changeset: changeset)
-  end
-end
+ use SharedShopper.Web, :controller
+ alias SharedShopper.User
+
+ def index(conn, _params) do
+   users = Repo.all(User)
+   render(conn, "index.html", users: users)
+ end
+
+ def new(conn, _params) do
+   changeset = User.changeset(%User{})
+   render(conn, "new.html", changeset: changeset)
+ end
+
+ def create(conn, %{"user" => user_params}) do
+   changeset = User.registration_changeset()
+   case Repo.insert(changeset) do
+     {:ok, user} ->
+       conn
+       |> put_flash(:info, "User created!")
+       |> redirect(to: user_path(conn, :index))
+     {:error, changeset} ->
+       conn
+       |> render("new.html", changeset: changeset)
+   end
+ end
+
+ def show(conn, %{"id" => id}) do
+   user = Repo.get(User, id)
+   changeset = User.changeset(user)
+   render(conn, "show.html", user: user, changeset: changeset)
+ end
+
+ def update(conn, %{"id" => id, "user" => user_params}) do
+   user = Repo.get(User, id)
+   changeset = User.registration_changeset()
+
+ case Repo.update(changeset) do
+     {:ok, user} ->
+       conn
+       |> put_flash(:info, "User updated")
+       |> redirect(to: user_path(conn, :index))
+     {:error, changeset} ->
+       conn
+       |> render("show.html", user: user, changeset: changeset)
+   end
+ end
 end
